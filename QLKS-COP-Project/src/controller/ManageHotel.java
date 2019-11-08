d0ac3e905bdc464f16c914ee33bb42cca1723b28
= <=>
2019-11-09 00:23:40 +0700

    add ManageHotel

---
+++ QLKS-COP-Project/src/controller/ManageHotel.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/manage-hotel")
+public class ManageHotel extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if(req.getSession().getAttribute("role").equals(0)) {// 0 is SysAdmin
+			
+			req.getRequestDispatcher("manage-hotel.jsp").forward(req, resp);
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+}
